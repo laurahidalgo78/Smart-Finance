8dc27260627611f0446d92182c60ce333d4c0224
Julian Dario Gonzalez Toledo <juliant.2001@outlook.com>
2024-11-28 22:30:41 -0500

    chore: Se crean vistas principales de la aplicación

---
+++ SmartFinance/src/screens/ingresos.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function ingresos(){
+    return (
+        <View>
+            <Text>Ingresos</Text>
+        </View>
+    )
+}
---
+++ SmartFinance/src/screens/inicioSesion.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function inicioSesion(){
+    return (
+        <View>
+            <Text>Inicio Sesion</Text>
+        </View>
+    )
+}
