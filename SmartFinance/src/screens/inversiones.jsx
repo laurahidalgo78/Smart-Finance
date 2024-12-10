25b584c4ce40aa13c66c33c31838d654b8392b0f
Julian Dario Gonzalez Toledo <juliant.2001@outlook.com>
2024-12-09 22:39:39 -0500

    chore: Se crea vista inferior de inversiones.

--- SmartFinance/src/screens/inversiones.jsx
+++ SmartFinance/src/screens/inversiones.jsx
@@ -1,9 +1,58 @@
-import { View, Text } from 'react-native';
+import { View , ScrollView, Image } from 'react-native';
+import { Icon , Card , Text, Button} from 'react-native-paper';
+import { SafeAreaView , SafeAreaProvider} from 'react-native-safe-area-context';
+import { estilosGlobal } from '../constants/estilosGlobal';
 
 export default function inversiones(){
     return (
-        <View>
-            <Text>Inversiones</Text>
-        </View>
+        <SafeAreaProvider>
+            <SafeAreaView style={estilosGlobal.contenedor}>
+                <ScrollView>
+                    <ScrollView>
+                        <View style={estilosGlobal.contenedorAccionesGlobales}>
+                            <View style={estilosGlobal.contenedorAcciones}>
+                                <View style={estilosGlobal.contenedorImagenEmpresa}>
+                                    <Image style={estilosGlobal.imagenEmpresa} source={require("../../assets/bitcoin-logo.png")}/>
+                                </View>
+                                <View style={estilosGlobal.contenedorBotonesCompraVenta}>
+                                    <Button mode="contained" style={estilosGlobal.botonVenta}>
+                                        Vender
+                                    </Button>
+                                    <Button mode="contained" style={estilosGlobal.botonCompra}>
+                                        Comprar
+                                    </Button>
+                                </View>
+                            </View>
+                            <View style={estilosGlobal.contenedorAcciones}>
+                                <View style={estilosGlobal.contenedorImagenEmpresa}>
+                                    <Image style={estilosGlobal.imagenEmpresa} source={require("../../assets/ethereum-logo.jpg")}/>
+                                </View>
+                                <View style={estilosGlobal.contenedorBotonesCompraVenta}>
+                                    <Button mode="contained" style={estilosGlobal.botonVenta}>
+                                        Vender
+                                    </Button>
+                                    <Button mode="contained" style={estilosGlobal.botonCompra}>
+                                        Comprar
+                                    </Button>
+                                </View>
+                            </View>
+                            <View style={estilosGlobal.contenedorAcciones}>
+                                <View style={estilosGlobal.contenedorImagenEmpresa}>
+                                    <Image style={estilosGlobal.imagenEmpresa} source={require("../../assets/tesla-logo.png")}/>
+                                </View>
+                                <View style={estilosGlobal.contenedorBotonesCompraVenta}>
+                                    <Button mode="contained" style={estilosGlobal.botonVenta}>
+                                        Vender
+                                    </Button>
+                                    <Button mode="contained" style={estilosGlobal.botonCompra}>
+                                        Comprar
+                                    </Button>
+                                </View>
+                            </View>
+                        </View>
+                    </ScrollView>
+                </ScrollView>             
+            </SafeAreaView>
+       </SafeAreaProvider>       
     )
 }
